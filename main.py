db9fa66d963144c248d3998d9456b24dee6dbc69
Lon1k <88199588+Lon1k@users.noreply.github.com>
2021-07-31 09:20:30 +0300

    Вернулся, лол

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from tkinter import *
 
 def click():
-    lbl.configure(text="Я жепросил")
+    res = txt.get()
+    print(f"Привет, {res}")
+    lbl.configure(text=f"Привет, {res}")
 
 window = Tk()
 window.title("Ха-ха")
 window.geometry("500x500")
 
 lbl = Label(window, 
-            text="qq",
-            font=("Arial Bold",50))
+            text="Привет",
+            font=(10))
 lbl.grid(column=0, row=0)
 
 
 btn = Button(window,
             text="Не нажимать",
-            bg="green",
-            fg="blue",
-            command=click)
-btn.grid(column=0, row=1)
+            command=click,
+            )
+btn.grid(column=1, row=0)
 
+txt = Entry(window,width=10)
+txt.grid(column=2,row=0)
 
 window.mainloop()
