f78f25b07b8a06682754c4f3d60601531082234c
Lon1k <88199588+Lon1k@users.noreply.github.com>
2021-07-31 09:01:14 +0300

    ушёл покакать
    я скоро

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
+def click():
+    lbl.configure(text="Я жепросил")
+
 window = Tk()
 window.title("Ха-ха")
 window.geometry("500x500")
@@ -8,5 +11,14 @@ lbl = Label(window,
             text="qq",
             font=("Arial Bold",50))
 lbl.grid(column=0, row=0)
+
+
+btn = Button(window,
+            text="Не нажимать",
+            bg="green",
+            fg="blue",
+            command=click)
+btn.grid(column=0, row=1)
+
 
 window.mainloop()
