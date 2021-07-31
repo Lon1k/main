f01152aa5747805f86900a54a23ad000741b506e
Lon1k <88199588+Lon1k@users.noreply.github.com>
2021-07-31 08:52:27 +0300

    +Label +Grid for label

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from tkinter import *
 
 window = Tk()
 window.title("Ха-ха")
+window.geometry("500x500")
+
+lbl = Label(window, 
+            text="qq",
+            font=("Arial Bold",50))
+lbl.grid(column=0, row=0)
+
 window.mainloop()
