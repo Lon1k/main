fdddc2ff9e0dcdfaf13f16673039fcce46315d39
Lon1k <88199588+Lon1k@users.noreply.github.com>
2021-07-31 10:22:09 +0300

    Combobox
    Практика использования комбобокса

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 from tkinter import *
-
+from tkinter.ttk import Combobox
 #window--------------------=+
 window = Tk()
 window.title("Ха-ха")
 window.geometry("500x500")
 #Функции-------------------=+
-def click():
-    res = txt.get()
-    lbl.configure(text=f"Привет, {res}")
+
 #Элементы------------------=+
-lbl = Label(window, 
-            text="Привет",)
-btn = Button(window,
-            text="Не нажимать",
-            command=click,)
-txt = Entry(window,width=10, state="disabled")
+combo = Combobox(window)
+combo["values"]=(1,2,3,4,5,"text")
+combo.current(0)
+combo.grid(column=0, row=0)
 #Grid----------------------=+
-lbl.grid(column=0, row=0)
-btn.grid(column=1, row=0)
-txt.grid(column=2,row=0)
 
 
 
